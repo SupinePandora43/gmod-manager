c8f07db18b337b215c640141ae185f326c004011
SupinePandora43 <xbox.account@bk.ru>
2019-06-17 20:39:04 +0500

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Gmod Manager
+## Features
+* ### Supports
+  * Addons
+  * Dupes
+  * Saves
+  * Collections
+* Download only if update available
+* Download progress
+* Custom configuration (main.cfg)
+## How-To-Use
+1. change paths in main.cfg (`gmad_path`, `gmod_path`, `temp_path`)
+2. enter in command line/prompt `python main.py -install` (steam workshop link)
+## Arguments
+* `-install` - argumant for providing workshop urls/id's. `python main.py -install https://steamcommunity.com/sharedfiles/filedetails/?id=1771611119`
+* `-nocheck` - argument for disabling checking for updates, addons in addons.json. `python main.py -nocheck -install https://steamcommunity.com/sharedfiles/filedetails/?id=1771611119`
+## Requirements
+* Python 3
+## Limitations
+* finds addons.json only in script folder
+* can't download collections through `-install` argument
+
+inspired by https://github.com/magnusjjj/gmadget
---
+++ main.py
@@ -0,0 +1,316 @@
+import requests
+import os
+import argparse
+import json
+import lzma
+import subprocess
+import shutil
+import sys
+from configparser import ConfigParser
+
+config = ConfigParser()
+if not os.path.exists("main.cfg"):
+    config.add_section("main")
+    config.set("main", "gmad_path", "gmad.exe")
+    config.set("main", "temp_path", "temp")
+    config.set("main", "gmod_path", ".")
+    with open("main.cfg", "w") as configFile:
+        config.write(configFile)
+        configFile.close()
+else:
+    config.read("main.cfg")
+
+folders = [
+    "addons", "dupes", "saves"
+]
+if not os.path.exists(config.get("main", "temp_path")):
+    os.makedirs(config.get("main", "temp_path"))
+for folder in folders:
+    if not os.path.exists(config.get("main", "gmod_path")+"/" + folder):
+        os.makedirs(config.get("main", "gmod_path")+"/" + folder)
+
+headers = {
+    "Content-type": "application/x-www-form-urlencoded",
+    "Accept": "text/plain"
+}
+
+
+class url_parser():
+    def getID(url):
+        import urllib.parse
+        steamID = urllib.parse.parse_qsl(url)[0][1]
+        return steamID
+
+
+class valid_fileName_generator():
+    fileName = None
+
+    def __init__(self, title):
+        """
+        generates valid fileName from steam workshop title
+
+        original code: https://gist.github.com/wassname/1393c4a57cfcbf03641dbc31886123b8
+        """
+        import unicodedata
+        import string
+        valid_filename_chars = "-_.[]() %s%s" % (string.ascii_letters, string.digits)
+        for r in ' ':
+            title = title.replace(r, '_')
+        self.fileName = unicodedata.normalize(
+            'NFKD', title).encode('ASCII', 'ignore').decode()
+        self.fileName = ''.join(
+            c for c in self.fileName if c in valid_filename_chars)
+        self.fileName = self.fileName[:255]
+
+
+class steam_object():
+    url = None
+    steamID = None
+    collectionResult = None
+    fileResult = None
+    collectionJSON = None
+    fileJSON = None
+    time_updated = None
+    directURL = None
+    previewURL = None
+    description = None
+    title = None
+    latestUpdate_time = None
+
+    def __init__(self, url="", time_updated=0):
+        self.url = url
+        self.steamID = url_parser.getID(self.url)
+        self.time_updated = time_updated
+        self.url = "https://steamcommunity.com/sharedfiles/filedetails/?id=" + \
+            str(self.steamID)
+        self.request()
+
+    def request(self):
+        self.collectionResult = requests.post(
+            "https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v0001/",
+            "collectioncount=1&publishedfileids[0]="+str(self.steamID),
+            headers=headers)
+        self.fileResult = requests.post(
+            "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v0001/",
+            "itemcount=1&publishedfileids[0]="+str(self.steamID),
+            headers=headers)
+        self.collectionJSON = json.loads(self.collectionResult.text)
+        self.fileJSON = json.loads(self.fileResult.text)
+        if self.steam_type() != None:
+            self.title = self.fileJSON['response']['publishedfiledetails'][0]['title']
+            self.description = self.fileJSON['response']['publishedfiledetails'][0]['description']
+            self.directURL = self.fileJSON['response']['publishedfiledetails'][0]['file_url']
+            self.previewURL = self.fileJSON['response']['publishedfiledetails'][0]['preview_url']
+            self.latestUpdate_time = self.fileJSON['response']['publishedfiledetails'][0]['time_updated']
+
+    def isLatest(self):
+        if self.latestUpdate_time <= self.time_updated:
+            return True
+        else:
+            return False
+
+    def steam_type(self):
+
+        if len(self.fileJSON['response']['publishedfiledetails'][0]) <= 2:
+            return None
+        elif str(self.fileJSON['response']['publishedfiledetails'][0]['tags'][0]['tag']) == "Addon" and str(self.fileJSON['response']['publishedfiledetails'][0]['filename']).endswith(".gma"):
+            return "addon"
+        elif str(self.fileJSON["response"]["publishedfiledetails"][0]["filename"]).startswith("creation/"):
+            if str(self.fileJSON['response']['publishedfiledetails'][0]['tags'][0]['tag']) == "Dupe":
+                return "dupe"
+            elif(str(self.fileJSON['response']['publishedfiledetails'][0]['tags'][0]['tag']) == "Save"):
+                return "save"
+        elif not self.collectionJSON['response']['collectiondetails'][0]['result'] == 9:
+            return "collection"
+
+
+# create addons.json if doesn't exists
+if not os.path.exists("addons.json"):
+    with open("addons.json", "w") as addonsFile:
+        addonsFile.write(json.dumps(
+            {"addons": [], "dupes": [], "saves": [], "collections": []}, indent=4))
+        addonsFile.close()
+# read addons.json
+with open("addons.json", "r") as addonsFile:
+    addons = json.loads(addonsFile.read())
+    addonsFile.close()
+
+
+def extract(steam_obj: steam_object):
+    path = config.get("main", "temp_path")+"/" + \
+        valid_fileName_generator(steam_obj.title).fileName
+    with lzma.open(path) as f:
+        with open(path + ".extracted", "wb+") as fout:
+            fout.write(f.read())
+            fout.close()
+        f.close()
+
+
+def installed(steam_obj: steam_object):
+    if steam_obj.steam_type() == "addon":
+        return os.path.exists(config.get("main", "gmod_path")+"/addons/"+valid_fileName_generator(steam_obj.title).fileName)
+    elif steam_obj.steam_type() == "dupe":
+        return os.path.exists(config.get("main", "gmod_path")+"/dupes/"+valid_fileName_generator(steam_obj.title).fileName+".dupe")
+    elif steam_obj.steam_type() == "save":
+        return os.path.exists(config.get("main", "gmod_path")+"/saves/"+valid_fileName_generator(steam_obj.title).fileName+".gms")
+
+
+def install(steam_obj: steam_object, collection: str = None, latest=False):
+    prefix = ""
+    indent = ""
+    if collection:
+        prefix = "├"
+        indent = "│ "
+        if latest:
+            prefix = "└"
+    print(prefix + steam_obj.title)
+    if (not steam_obj.isLatest()) or (not installed(steam_obj)):
+        # print(indent+"├ downloading")
+        r = requests.get(steam_obj.directURL, stream=True)
+        with open(config.get("main", "temp_path")+"/"+valid_fileName_generator(steam_obj.title).fileName, "wb") as workshopFile:
+            total_length = r.headers.get('content-length')
+            if total_length is None:
+                workshopFile.write(r.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+                for data in r.iter_content(chunk_size=4096):
+                    dl += len(data)
+                    workshopFile.write(data)
+                    done = int(50 * dl / total_length)
+                    sys.stdout.write("\r"+indent+"├ downloading [%s%s]" %
+                                     ('=' * done, '┈' * (50-done)))
+                    sys.stdout.flush()
+                print("")
+            workshopFile.close()
+        print(indent + "├ extracting")
+        extract(steam_obj)
+        os.remove(config.get("main", "temp_path")+"/" +valid_fileName_generator(steam_obj.title).fileName)
+        if steam_obj.steam_type() == "addon":
+            print(indent + "└ extracting via gmad")
+            subprocess.check_output([config.get("main", "gmad_path"), "extract", "-file", config.get("main", "temp_path")+"/" +
+                                     valid_fileName_generator(steam_obj.title).fileName + ".extracted", "-out", config.get("main", "gmod_path")+"/addons/"+valid_fileName_generator(steam_obj.title).fileName])
+        elif steam_obj.steam_type() == "dupe":
+            shutil.copy(config.get("main", "temp_path")+"/"+valid_fileName_generator(steam_obj.title).fileName + ".extracted",
+                        config.get("main", "gmod_path")+"/dupes/"+valid_fileName_generator(steam_obj.title).fileName+".dupe")
+            preview = requests.get(steam_obj.previewURL)
+            with open(config.get("main", "gmod_path")+"/dupes/"+valid_fileName_generator(steam_obj.title).fileName + ".jpg", "wb") as previewFile:
+                previewFile.write(preview.content)
+                previewFile.close()
+        elif steam_obj.steam_type() == "save":
+            shutil.copy(config.get("main", "temp_path")+"/"+valid_fileName_generator(steam_obj.title).fileName + ".extracted",
+                        config.get("main", "gmod_path")+"/saves/"+valid_fileName_generator(steam_obj.title).fileName+".gms")
+            preview = requests.get(steam_obj.previewURL)
+            with open(config.get("main", "gmod_path")+"/saves/"+valid_fileName_generator(steam_obj.title).fileName + ".jpg", "wb") as previewFile:
+                previewFile.write(preview.content)
+                previewFile.close()
+        os.remove(config.get("main", "temp_path")+"/" +valid_fileName_generator(steam_obj.title).fileName + ".extracted")
+    else:
+        print(indent + "└ latest")
+
+
+parser = argparse.ArgumentParser(
+    description='Tool for managing Gmod Steam content')
+parser.add_argument("-nocheck",
+                    help="Install/Check content only provided via 'install' arg", action="store_true")
+
+parser.add_argument("-install",
+                    help="Install new content", type=str, nargs='+', metavar='ID')
+args = vars(parser.parse_args())
+
+
+def installARGS(url):
+    new_steam_object = steam_object(url)
+    category = addons[new_steam_object.steam_type() + "s"]
+    install(new_steam_object)
+    new_object = {"title": new_steam_object.title,
+                  "description": new_steam_object.description,
+                  "preview": new_steam_object.previewURL,
+                  "url": new_steam_object.url,
+                  "time_updated": new_steam_object.latestUpdate_time}
+    category.append(new_object)
+
+
+if args["install"]:
+    for steam in args["install"]:
+        if str(steam).isdigit():
+            installARGS(
+                "https://steamcommunity.com/sharedfiles/filedetails/?id="+steam)
+        elif str(steam):
+            installARGS(steam)
+        else:
+            pass
+if not args["nocheck"]:
+    for addon in addons["addons"]:
+        addonURL = addon["url"]
+        addonTimeUpdated = addon["time_updated"]
+        addonSTEAM = steam_object(addonURL, addonTimeUpdated)
+        addon["title"] = addonSTEAM.title
+        addon["description"] = addonSTEAM.description
+        addon["preview"] = addonSTEAM.previewURL
+        addon["url"] = addonSTEAM.url
+        install(addonSTEAM)
+        addon["time_updated"] = addonSTEAM.latestUpdate_time
+
+    for dupe in addons["dupes"]:
+        dupeURL = dupe["url"]
+        dupeTimeUpdated = dupe["time_updated"]
+        dupeSTEAM = steam_object(dupeURL, dupeTimeUpdated)
+        dupe["title"] = dupeSTEAM.title
+        dupe["description"] = dupeSTEAM.description
+        dupe["preview"] = dupeSTEAM.previewURL
+        dupe["url"] = dupeSTEAM.url
+        install(dupeSTEAM)
+        dupe["time_updated"] = dupeSTEAM.latestUpdate_time
+
+    for save in addons["saves"]:
+        saveURL = save["url"]
+        saveTimeUpdated = save["time_updated"]
+        saveSTEAM = steam_object(saveURL, saveTimeUpdated)
+        save["title"] = saveSTEAM.title
+        save["description"] = saveSTEAM.description
+        save["preview"] = saveSTEAM.previewURL
+        save["url"] = saveSTEAM.url
+        install(saveSTEAM)
+        save["time_updated"] = saveSTEAM.latestUpdate_time
+
+    for i in range(len(addons["collections"])):
+        collection = addons["collections"][i]
+        collectionSTEAM = steam_object(collection["url"])
+        print(collectionSTEAM.title)
+        addons["collections"][i]["url"] = collectionSTEAM.url
+        addons["collections"][i]["title"] = collectionSTEAM.title
+        addons["collections"][i]["description"] = collectionSTEAM.description
+        addons["collections"][i]["preview"] = collectionSTEAM.previewURL
+        newTimes = {}
+        for iID in range(len(collectionSTEAM.collectionJSON['response']['collectiondetails'][0]['children'])):
+            addon = collectionSTEAM.collectionJSON["response"]['collectiondetails'][0]["children"][iID]
+            try:
+                newTimes[addon["publishedfileid"]
+                         ] = collection["childrens"][addon["publishedfileid"]]
+            except:
+                newTimes[addon["publishedfileid"]] = 0
+
+            addonSTEAM = steam_object(
+                "https://steamcommunity.com/sharedfiles/filedetails/?id="+addon["publishedfileid"], newTimes[addon["publishedfileid"]])
+            install(addonSTEAM, collection=collectionSTEAM.title, latest=(iID == len(
+                collectionSTEAM.collectionJSON['response']['collectiondetails'][0]['children'])))
+            newTimes[addon["publishedfileid"]] = addonSTEAM.latestUpdate_time
+        addons["collections"][i]["time_updated"] = collectionSTEAM.latestUpdate_time
+        addons["collections"][i]["childrens"] = newTimes
+
+# for i in range(len(addons["collections"])):
+#     collection = addons["collections"][i]
+#     collectionSTEAM = steam_object(collection["url"])
+#     for addon in collectionSTEAM.collectionJSON['response']['collectiondetails'][0]['"children"']:
+#         addonSTEAM = steam_object(
+#             "https://steamcommunity.com/sharedfiles/filedetails/?id="+addon["publishedfileid"])
+#         print(addonSTEAM.title)
+#         addons['collections'][i]['children'][addonSTEAM.steamID] = addonSTEAM.latestUpdate_time
+
+with open("addons.json", "w") as addonsFile:
+    addonsFile.write(json.dumps(addons, indent=4))
+    addonsFile.close()
+# https://steamcommunity.com/sharedfiles/filedetails/?id=1716648512 dupe
+# https://steamcommunity.com/sharedfiles/filedetails/?id=1767675383 save
+# https://steamcommunity.com/sharedfiles/filedetails/?id=675560712  addon
